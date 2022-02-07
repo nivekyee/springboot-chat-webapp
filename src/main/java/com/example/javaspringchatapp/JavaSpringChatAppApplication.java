6c4bf460b47662efa7d0230051501304be30acc3
Kevin Yee <nivekyee@hotmail.com>
2022-02-07 18:03:39 +1300

    Initial commit

---
+++ src/main/java/com/example/javaspringchatapp/JavaSpringChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringChatAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringchatapp/JavaSpringChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringChatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
